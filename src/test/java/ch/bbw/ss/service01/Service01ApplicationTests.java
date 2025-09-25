03ffed16ef3b007bc4d8d1c3c9bd0bf90ad9d9ea
Said Simokovic <said.simokovic@digitecgalaxus.ch>
2025-09-25 16:22:59 +0200

    add project

---
+++ src/test/java/ch/bbw/ss/service01/Service01ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.ss.service01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Service01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
